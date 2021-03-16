c32df44967425a1d07d79b37400938816ec8902f
Linnéa <linnea@klangby.com>
2021-03-16 11:09:24 +0100

    pokemons

--- ApiServer/Controllers/PokemonController.cs
+++ ApiServer/Controllers/PokemonController.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace ApiServer.Controllers
 {
@@ -8,10 +9,16 @@ namespace ApiServer.Controllers
     [Route("api/pokemon")]
     public class PokemonController: ControllerBase
     {
+        static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "Pikachu"},
+            new Pokemon() {Name = "Charmander"}
+        };
+
         [HttpGet]
         public ActionResult Get() 
         {
-            return Ok("Hej");
+            return Ok(pokemons[0]);
         }
     }
 }
---
+++ ApiServer/Pokemon.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ApiServer
+{
+    public class Pokemon
+    {
+        public string Name {get; set;}
+        public int Level {get; set;} = 1;
+    }
+}
