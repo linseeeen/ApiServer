6044901d95c83636d066e48e17a3306951fcbac2
Linnéa <linnea@klangby.com>
2021-03-16 10:34:19 +0100

    ApiServer

---
+++ ApiServer/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+
+    [ApiController]
+    [Route("api/pokemon")]
+    public class PokemonController: ControllerBase
+    {
+        public ActionResult private void OnDrawGizmosSelected() 
+        {
+            return Ok("Hej");
+        }
+    }
+}
